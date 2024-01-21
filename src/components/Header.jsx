c279b74d1102798699d465cc526f3a0876a38c56
SHoogenboom <stijnhoog51@gmail.com>
2024-01-21 21:50:15 +0100

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import style from './header.module.scss';
 
 const Header = props => (
-	<section className={style.header}> Scouting Rambonnet Groep </section>
+	<section className={style.header}> Scouting Rambonnet Groep! </section>
 );
 export default Header;
